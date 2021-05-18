594e56b4f28740c0a35f0c5c949dd1d7390dd6fc
CiberTurtle <58660466+CiberTurtle@users.noreply.github.com>
2021-05-18 13:13:48 -0500

    Less Bounce

--- Source/GAME/Components/Items/CGrenadeLauncher.cs
+++ Source/GAME/Components/Items/CGrenadeLauncher.cs
@@ -8,7 +8,7 @@ namespace GAME.Components.Items
 	{
 		public override void SpawnProjectile(DamageInfo info, Vector2 position, float rotation)
 		{
-			Spawn(new Entity(new CRigidbody(new Vector2(0.75f, 0.9f)), new CGrenade(info, relitivePath)), position, rotation);
+			Spawn(new Entity(new CRigidbody(new Vector2(0.33f, 0.33f)), new CGrenade(info, relitivePath)), position, rotation);
 		}
 	}
 }
